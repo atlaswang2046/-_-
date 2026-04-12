3f25c6f63a0cf72a2f42bff85dbd6f2a218c6a65
atlaswang2046 <atlaswang2046@users.noreply.github.com>
2026-04-12 10:40:28 +0800

    chore: save latest manual updates to P4.6 project statistics page

--- parse_pen.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('V1_原型图_01统一预约工作台.pen', 'utf8'));
-
-console.log("Top-level frames/pages:");
-data.children.forEach(child => {
-  console.log(`- ${child.name} (id: ${child.id})`);
-});
